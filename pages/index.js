6ea746247fcf9d5b33708cb3033b8c73e4319d94
Jordan Lei <thunderflash.lei@gmail.com>
2019-12-20 02:54:43 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Index = () => (
-    <div style={{height: "500px", backgrounImage: "url(" + require("../public/static/logo-cmu.png") + ")"}}>
+    <div style={{height: "500px", backgroundImage: "url(" + require("../public/static/logo-cmu.png") + ")"}}>
 
     </div>
   );
